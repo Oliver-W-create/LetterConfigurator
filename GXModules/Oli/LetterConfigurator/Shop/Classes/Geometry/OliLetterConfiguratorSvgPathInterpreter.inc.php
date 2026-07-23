3cacb427e8c7721aff9a1f60eddd0ca6110d900c
Dein NameOliver-W-create <info@mediendesignkontor.de>
2026-07-23 21:21:01 +0200

    M5.5.2: Interpret basic SVG path commands

--- GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorSvgPathInterpreter.inc.php
+++ GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorSvgPathInterpreter.inc.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- * Interprets normalized SVG path commands as geometry.
- *
- * Geometry interpretation is intentionally deferred to later milestones.
+ * Resolves normalized SVG path commands to their resulting points.
  */
 class OliLetterConfiguratorSvgPathInterpreter
 {
@@ -20,12 +18,74 @@ class OliLetterConfiguratorSvgPathInterpreter
             return [];
         }
 
-        foreach ($commands as $command) {
-            throw new OliLetterConfiguratorGeometryException(
-                'SVG path interpretation not implemented yet.'
-            );
+        $points = [];
+        $currentPoint = null;
+        $subPathStart = null;
+
+        foreach ($commands as $pathCommand) {
+            $command = $pathCommand->getCommand();
+            $parameters = $pathCommand->getParameters();
+            $relative = $pathCommand->isRelative();
+            $fromPoint = $currentPoint;
+
+            if ($command === 'M') {
+                $baseX = $relative && $currentPoint !== null ? $currentPoint['x'] : 0.0;
+                $baseY = $relative && $currentPoint !== null ? $currentPoint['y'] : 0.0;
+                $currentPoint = [
+                    'x' => $relative ? $baseX + $parameters[0] : $parameters[0],
+                    'y' => $relative ? $baseY + $parameters[1] : $parameters[1],
+                ];
+                $subPathStart = $currentPoint;
+            } else {
+                if ($currentPoint === null || $subPathStart === null) {
+                    throw new OliLetterConfiguratorGeometryException(
+                        'SVG path command ' . $command . ' cannot be interpreted before M or m.'
+                    );
+                }
+
+                switch ($command) {
+                    case 'L':
+                        $currentPoint = [
+                            'x' => $relative ? $currentPoint['x'] + $parameters[0] : $parameters[0],
+                            'y' => $relative ? $currentPoint['y'] + $parameters[1] : $parameters[1],
+                        ];
+                        break;
+
+                    case 'H':
+                        $currentPoint = [
+                            'x' => $relative ? $currentPoint['x'] + $parameters[0] : $parameters[0],
+                            'y' => $currentPoint['y'],
+                        ];
+                        break;
+
+                    case 'V':
+                        $currentPoint = [
+                            'x' => $currentPoint['x'],
+                            'y' => $relative ? $currentPoint['y'] + $parameters[0] : $parameters[0],
+                        ];
+                        break;
+
+                    case 'Z':
+                        $currentPoint = $subPathStart;
+                        break;
+
+                    default:
+                        throw new OliLetterConfiguratorGeometryException(
+                            'SVG path command ' . $command . ' is not implemented yet.'
+                        );
+                }
+            }
+
+            $points[] = [
+                'command' => $command,
+                'from' => $command === 'M' ? null : $fromPoint,
+                'to' => [
+                    'x' => $currentPoint['x'],
+                    'y' => $currentPoint['y'],
+                ],
+            ];
         }
 
-        return [];
+        return $points;
     }
 }
