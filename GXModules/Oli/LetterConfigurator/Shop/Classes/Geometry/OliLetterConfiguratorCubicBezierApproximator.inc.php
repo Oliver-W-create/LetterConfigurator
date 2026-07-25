c44690cdb80a686e72ca9eab62c800369c8ff12d
Dein NameOliver-W-create <info@mediendesignkontor.de>
2026-07-25 10:05:51 +0200

    M11.3.1: Calculate cubic bezier sample points

--- GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorCubicBezierApproximator.inc.php
+++ GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorCubicBezierApproximator.inc.php
@@ -12,9 +12,7 @@ class OliLetterConfiguratorCubicBezierApproximator
      * @param OliLetterConfiguratorPoint $endPoint
      * @param int                        $segmentCount
      *
-     * @return void
-     *
-     * @throws OliLetterConfiguratorGeometryException
+     * @return OliLetterConfiguratorPoint[]
      */
     public function approximate(
         OliLetterConfiguratorPoint $startPoint,
@@ -23,8 +21,28 @@ class OliLetterConfiguratorCubicBezierApproximator
         OliLetterConfiguratorPoint $endPoint,
         int $segmentCount
     ) {
-        throw new OliLetterConfiguratorGeometryException(
-            'Cubic Bézier approximation is not implemented yet.'
-        );
+        $points = [];
+
+        for ($index = 0; $index <= $segmentCount; $index++) {
+            $t = $index / $segmentCount;
+            $oneMinusT = 1.0 - $t;
+            $startFactor = $oneMinusT * $oneMinusT * $oneMinusT;
+            $controlFactor1 = 3.0 * $oneMinusT * $oneMinusT * $t;
+            $controlFactor2 = 3.0 * $oneMinusT * $t * $t;
+            $endFactor = $t * $t * $t;
+
+            $x = ($startFactor * $startPoint->getX())
+                + ($controlFactor1 * $controlPoint1->getX())
+                + ($controlFactor2 * $controlPoint2->getX())
+                + ($endFactor * $endPoint->getX());
+            $y = ($startFactor * $startPoint->getY())
+                + ($controlFactor1 * $controlPoint1->getY())
+                + ($controlFactor2 * $controlPoint2->getY())
+                + ($endFactor * $endPoint->getY());
+
+            $points[] = new OliLetterConfiguratorPoint($x, $y);
+        }
+
+        return $points;
     }
 }
