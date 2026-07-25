4bd4be47044177bdddd28ae50680bc22a884ee0e
Dein NameOliver-W-create <info@mediendesignkontor.de>
2026-07-25 17:12:28 +0200

    M12.2: Resolve quadratic bezier control points

--- GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorQuadraticBezierInterpreter.inc.php
+++ GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorQuadraticBezierInterpreter.inc.php
@@ -5,6 +5,16 @@
  */
 class OliLetterConfiguratorQuadraticBezierInterpreter
 {
+    /** @var OliLetterConfiguratorPointTranslator */
+    private $pointTranslator;
+
+    public function __construct(
+        ?OliLetterConfiguratorPointTranslator $pointTranslator = null
+    ) {
+        $this->pointTranslator = $pointTranslator
+            ?: new OliLetterConfiguratorPointTranslator();
+    }
+
     /**
      * @param OliLetterConfiguratorPoint          $startPoint
      * @param OliLetterConfiguratorSvgPathCommand $pathCommand
@@ -17,6 +27,29 @@ class OliLetterConfiguratorQuadraticBezierInterpreter
         OliLetterConfiguratorPoint $startPoint,
         OliLetterConfiguratorSvgPathCommand $pathCommand
     ) {
+        $parameters = $pathCommand->getParameters();
+        if ($pathCommand->isRelative()) {
+            $controlPoint = $this->pointTranslator->translate(
+                $startPoint,
+                $parameters[0],
+                $parameters[1]
+            );
+            $endPoint = $this->pointTranslator->translate(
+                $startPoint,
+                $parameters[2],
+                $parameters[3]
+            );
+        } else {
+            $controlPoint = new OliLetterConfiguratorPoint(
+                $parameters[0],
+                $parameters[1]
+            );
+            $endPoint = new OliLetterConfiguratorPoint(
+                $parameters[2],
+                $parameters[3]
+            );
+        }
+
         throw new OliLetterConfiguratorGeometryException(
             'SVG path command Q is not implemented yet.'
         );
