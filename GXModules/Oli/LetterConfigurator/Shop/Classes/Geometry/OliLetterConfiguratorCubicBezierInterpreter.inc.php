62f1857c2fbbb51acbcdc702f0c139b00a517406
Dein NameOliver-W-create <info@mediendesignkontor.de>
2026-07-25 09:27:14 +0200

    M11.2.2: Resolve cubic bezier control points

--- GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorCubicBezierInterpreter.inc.php
+++ GXModules/Oli/LetterConfigurator/Shop/Classes/Geometry/OliLetterConfiguratorCubicBezierInterpreter.inc.php
@@ -5,6 +5,16 @@
  */
 class OliLetterConfiguratorCubicBezierInterpreter
 {
+    /** @var OliLetterConfiguratorPointTranslator */
+    private $pointTranslator;
+
+    public function __construct(
+        ?OliLetterConfiguratorPointTranslator $pointTranslator = null
+    ) {
+        $this->pointTranslator = $pointTranslator
+            ?: new OliLetterConfiguratorPointTranslator();
+    }
+
     /**
      * @param OliLetterConfiguratorPoint          $startPoint
      * @param OliLetterConfiguratorSvgPathCommand $pathCommand
@@ -17,6 +27,39 @@ class OliLetterConfiguratorCubicBezierInterpreter
         OliLetterConfiguratorPoint $startPoint,
         OliLetterConfiguratorSvgPathCommand $pathCommand
     ) {
+        $parameters = $pathCommand->getParameters();
+
+        if ($pathCommand->isRelative()) {
+            $controlPoint1 = $this->pointTranslator->translate(
+                $startPoint,
+                $parameters[0],
+                $parameters[1]
+            );
+            $controlPoint2 = $this->pointTranslator->translate(
+                $startPoint,
+                $parameters[2],
+                $parameters[3]
+            );
+            $endPoint = $this->pointTranslator->translate(
+                $startPoint,
+                $parameters[4],
+                $parameters[5]
+            );
+        } else {
+            $controlPoint1 = new OliLetterConfiguratorPoint(
+                $parameters[0],
+                $parameters[1]
+            );
+            $controlPoint2 = new OliLetterConfiguratorPoint(
+                $parameters[2],
+                $parameters[3]
+            );
+            $endPoint = new OliLetterConfiguratorPoint(
+                $parameters[4],
+                $parameters[5]
+            );
+        }
+
         throw new OliLetterConfiguratorGeometryException(
             'SVG path command ' . $pathCommand->getCommand() . ' is not implemented yet.'
         );
